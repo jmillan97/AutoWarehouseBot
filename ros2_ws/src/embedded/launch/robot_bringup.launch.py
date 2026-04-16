dff1eac10508843187de02fdcee236ef5d596e26
Juan Millan <jmillan1997@gmail.com>
2026-04-16 16:56:20 -0500

    updated bringuo.launch

--- ros2_ws/src/embedded/launch/robot_bringup.launch.py
+++ ros2_ws/src/embedded/launch/robot_bringup.launch.py
@@ -28,9 +28,9 @@ Usage (on Pi):
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.actions import DeclareLaunchArgument, TimerAction, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration, Command
-from launch_ros.actions import Node, RegisterEventHandler, TimerAction
+from launch_ros.actions import Node, TimerAction
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.event_handlers import OnProcessExit
 
